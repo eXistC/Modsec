d0bceedaddd71abe4290cf633a0e91c8c9cf8bb5
boss10801 <boss101801@gmail.com>
2025-03-04 21:15:35 +0700

    Update unfininshed Data structure

--- clientside/CipherAlgo/Operation/DataStr/DataStr.go
+++ clientside/CipherAlgo/Operation/DataStr/DataStr.go
@@ -1,8 +1,12 @@
 package DataStr
 
-type Data struct {
+type ResData struct {
 	EncryptedHp1_HpR   string `json:"encrypted_hp1_hpr"`
 	EncryptedIteration string `json:"encrypted_iteration"`
 	ProtectedVaultKey  string `json:"protected_vault_key"`
 	Email              string `json:"email"`
+}
+
+type LoginData struct {
+	EncryptedHp1_HpR string `json:"encrypted_hp1_hpr"`
 }

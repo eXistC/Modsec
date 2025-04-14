a5a093d0c2c0d8a88eaa73503ca652e6b54adeb7
boss10801 <boss101801@gmail.com>
2025-04-14 13:16:44 +0700

    Added keymaster package to handle all key needed

---
+++ clientside/CipherAlgo/keymaster/keymaster.go
@@ -0,0 +1,28 @@
+package keymaster
+
+import "Modsec/clientside/CipherAlgo/utils"
+
+// This will be critical part for encryption act similar to global varible but only function that has this package
+// can access these key
+
+var Masterkey []byte
+var Vaultkey []byte
+var Sessionkey []byte
+
+func GetSessionkey() []byte {
+	return Sessionkey
+}
+
+func GetMasterkey() []byte {
+	return Masterkey
+}
+
+func GetVaultkey() []byte {
+	return Vaultkey
+}
+
+func ExtractVaultKey(EnVaultkey []byte, key []byte, IV []byte) error {
+	var err error
+	Vaultkey, err = utils.DecryptAES256GCM(EnVaultkey, key, IV)
+	return err
+}
